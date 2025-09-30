e8b6bd1f058cc1b2bec35f42f89e75690f4fca61
Guy  Cizner <46866327+ciznerguy@users.noreply.github.com>
2025-09-30 12:06:27 +0300

    Add project files.

---
+++ Point.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOriented
+{
+    public class Point
+    {
+        private int x;
+        private int y;
+        private static int numPoints;
+        private const int MaxDistanceOrigin = 10;
+
+
+        public double DistanceFromOrigin()
+        {
+            return Math.Sqrt(x * x + y * y);
+        }
+
+        /*פעולה בונה.
+         * יוצרת נקודה חדשה,
+         * כל עוד מרחקה מהראשית
+        אינו גדול מהמרחק המקסימלי,
+        אחרת מודפסת שגיאה
+         */
+
+        public Point(int x, int y)
+        {
+            Console.WriteLine("main");
+            if (this.DistanceFromOrigin() <= MaxDistanceOrigin)
+            {
+                this.x = x;
+                this.y = y;
+                numPoints++;
+            }
+            else
+            {
+                Console.WriteLine("Error: Point is too far from the origin.");
+            }
+        }
+        /*
+         * הוסיפו את הפעולה הבונה הבאה
+         * כך שתשרשר את הפעולה הבונה הקיימת:
+         */
+
+
+        public Point() : this(0, 0)
+        {
+            Console.WriteLine("empty");
+        }
+
+        /*
+         * הוסיפו את 
+         * הפעולה הבונה הבאה כך שתשרשר את 
+         * הפעולה הבונה הראשונה שכתבתם:
+
+פעולה בונה מעתיקה
+            */
+        public Point(Point other) : this(other.x, other.y)
+        {
+            Console.WriteLine("copy");
+        }
+
+
+        public static int GetNumPoints()
+        {
+            return numPoints;
+        }
+        public bool IsEqual(Point other)
+        {
+            return this.AreValuesEqaul(other.x, other.y);
+        }
+        public bool AreValuesEqaul(int x, int y)
+        {
+            return this.x == x && this.y == y;
+        }
+        public override string ToString()
+        {
+            return $"({x}, {y})";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOriented
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            Point p1 = new Point(3, 4);
+            Point p2 = new Point(p1);
+            Point p3 = new Point(); // This should print an error message
+            Console.WriteLine($"Number of points created: {Point.GetNumPoints()}");
+            Console.WriteLine($"p1: {p1}");
+            Console.WriteLine($"p2: {p2}");
+            Console.WriteLine($"p3: {p3}");
+        
+            Console.WriteLine($"p2 is equal to p3: {p2.IsEqual(p3)}");
+            Console.WriteLine($"p1 is equal to p2: {p1.IsEqual(p2)}");
+        }
+    }
+}
